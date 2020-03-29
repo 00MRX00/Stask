4d63106bb7908b775108f70bb5cb6a12d9351e1e
00MRX00 <17515560@mail.ru>
2020-03-29 16:04:21 +0300

    Added models: Project, ProjectUsers, Task, TaskUsers, Todo

--- Stask/apps/signUp/admin.py
+++ Stask/apps/signUp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import User, UserLogPass
+from .models import User, UserLogPass, Project, ProjectUsers, Task, TaskUsers, Todo
 
 admin.site.register(User)
 admin.site.register(UserLogPass)
+admin.site.register(Project)
+admin.site.register(ProjectUsers)
+admin.site.register(Task)
+admin.site.register(TaskUsers)
+admin.site.register(Todo)
--- Stask/apps/signUp/models.py
+++ Stask/apps/signUp/models.py
@@ -39,7 +39,7 @@ class User(models.Model):
 		verbose_name_plural = 'Пользователи'
 
 	
-# q = User(user_name="Александр", user_surname="Олейников", user_patronymic="Павлович", user_email="17515560@mail.ru", user_birthdate=datetime(2000,5,31,0,0,0,0,tzinfo=pytz.UTC), user_reg_date=timezone.now())
+# q = User(user_name="Тест", user_surname="Тестов", user_patronymic="Тестович", user_email="test@test.ru", user_birthdate=datetime(2010,10,10,0,0,0,0,tzinfo=pytz.UTC), user_reg_date=timezone.now())
 
 class UserLogPass(models.Model):
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -54,3 +54,76 @@ class UserLogPass(models.Model):
 		verbose_name = 'Логин/Пароль'
 		verbose_name_plural = 'Логины/Пароли'
 
+
+class Project(models.Model):
+	id = models.AutoField(primary_key=True)
+	project_title = models.CharField('Название проекта', max_length = 255)
+	project_description = models.TextField('Описание проекта', max_length = 3000)
+	project_creation_date =  models.DateTimeField('Дата создания проекта')
+
+	def __str__(self):
+		return self.project_title
+
+	class Meta:
+		verbose_name = 'Проект'
+		verbose_name_plural = 'Проекты'
+
+class ProjectUsers(models.Model):
+	id = models.AutoField(primary_key=True)
+	project = models.ForeignKey(Project, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	user_type = models.CharField('Тип пользователя', max_length = 50, default = "undistributed")
+
+	def __str__(self):
+		string = str(self.project) + " - " + str(self.user)
+		return string
+
+	class Meta:
+		verbose_name = 'Участник проекта'
+		verbose_name_plural = 'Участники проектов'
+
+
+class Task(models.Model):
+	id = models.AutoField(primary_key=True)
+	project = models.ForeignKey(Project, on_delete = models.CASCADE)
+	task_title = models.CharField('Заголовок задания', max_length = 255)
+	task_description = models.TextField('Описание задания', max_length = 3000)
+	task_creation_date = models.DateTimeField('Дата создания задания')
+	start_date = models.DateTimeField('Начало выполнения задания')
+	end_date = models.DateTimeField('Конец выполнения задания')
+	is_public = models.BooleanField('Публичное задание', default=False)
+	done = models.BooleanField('Задание выполнено', default=False)
+
+	def __str__(self):
+		return str(self.project) + " - " + self.task_title
+
+	class Meta:
+		verbose_name = 'Задание'
+		verbose_name_plural = 'Задания'
+
+
+class TaskUsers(models.Model):
+	id = models.AutoField(primary_key=True)
+	task = models.ForeignKey(Task, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+	def __str__(self):
+		string = str(self.task) + " - " + str(self.user)
+		return string
+
+	class Meta:
+		verbose_name = 'Ответственный за задание'
+		verbose_name_plural = 'Ответственные за задания'
+
+class Todo(models.Model):
+	id = models.AutoField(primary_key=True)
+	task = models.ForeignKey(Task, on_delete = models.CASCADE)
+	text = models.TextField('Текст TODO', max_length = 3000)
+	done = models.BooleanField('TODO выполнено', default=False)
+
+	def __str__(self):
+		if len(str(self.text)) > 30:
+			string = str(self.task) + " - " + str(self.text)[:30]
+		else:
+			string = str(self.task) + " - " + str(self.text)
+		return string
